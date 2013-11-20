6f022abbf0bf5947ff40db71dd2f3cfd1c86b48d
dacm26 <dacm26@gmail.com>
2013-11-19 21:12:29 -0600

    Create Listo

---
+++ author.php
@@ -0,0 +1,97 @@
+<html lang="es">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Add Books</title>
+		<link rel="shortcut icon" href="bk.png">
+		<link rel="stylesheet" type="text/css" href="style.css"> 
+		<script type="text/javascript">
+        function OnlyNumbers (event) {
+            var keyCode = ('which' in event) ? event.which : event.keyCode;
+
+            isNumeric = ((keyCode >= 48 && keyCode <= 57 ) ||
+                        (keyCode >= 96  && keyCode <= 105 ) ||keyCode==8);
+            modifiers = (event.altKey || event.ctrlKey || event.shiftKey);
+
+            return !(isNumeric && modifiers) && isNumeric;
+        }
+    	</script>
+    	<script type="text/javascript">
+        function OnlyLeters (event) {
+            var keyCode = ('which' in event) ? event.which : event.keyCode;
+
+            isNumeric = ((keyCode >= 48  && keyCode <= 57 ) ||
+                        (keyCode >= 96  && keyCode <= 105 ) ||keyCode==8 || keyCode == 32);
+            modifiers = (event.altKey || event.ctrlKey || event.shiftKey);
+
+            return (!(isNumeric && modifiers) && !isNumeric) || keyCode==8 || keyCode==32;
+        }
+    </script>
+	</head>
+	<body class="bar">
+		<?php
+			echo '		<div>
+		<ul class = "nav">
+			<li>
+				<a href="index.html">Home<span class = "flecha">&#9660</span></a>
+			</li>
+			<li>
+				<a href="#">Books<span class = "flecha">&#9660</span></a>
+				<ul>
+					<li><a href="book.php">Add Book<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">Remove Book<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">Update Book<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">List Book<span class = "flecha">&#9660</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="#">Authors<span class = "flecha">&#9660</span></a>
+				<ul>
+					<li><a href="author.php">Add Author<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">Remove Author<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">Update Author<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">List Authors<span class = "flecha">&#9660</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="#">Book Authors<span class = "flecha">&#9660</span></a>
+				<ul>
+					<li><a href="bookauthor.php">Add Book Author<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">Remove Book Author<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">Update Book Author<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">List Book Authors<span class = "flecha">&#9660</span></a></li>
+				</ul>
+			</li>	
+			<li>
+				<a href="#">Editorial<span class = "flecha">&#9660</span></a>
+				<ul>
+					<li><a href="editorial.php">Add Editorial<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">Remove Editorial<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">Update Editorial<span class = "flecha">&#9660</span></a></li>
+					<li><a href="#">List Editorials<span class = "flecha">&#9660</span></a></li>
+				</ul>
+			</li>				
+		</ul>
+		</div>';
+			$con = mysqli_connect("localhost","root","daniel12031994","bookstore");
+			if (mysqli_connect_errno($con)) {
+				echo "Error";
+			}
+			if($_SERVER['REQUEST_METHOD'] == 'GET'){//Para cuando recargue la pagina y solo quiere refrescarlo o si quiere que pase algo cuando cargue la pagina para read 
+			}
+			if($_SERVER['REQUEST_METHOD'] == 'POST'){//Para cuando recargue la pagina y le envia la data que ingreso el usuario para insert delete
+				$query = "INSERT INTO author(idauthor,nameauthor,nationality) VALUES (".$_POST['id_author'].",'".$_POST['name_author']."','".$_POST['nationality']."');";
+				if(!mysqli_query($con,$query)){
+				echo '<script>
+						alert("Ya hay autores con ese Id");
+					  </script>';
+				}
+			}
+			echo '<form method="post">';
+			echo '<div class="mb" id ="add_books" >'."<strong>Id</strong>".'<input id ="ab_fields_1" type="text" name="id_author" placeholder="Author Id" onkeydown="return OnlyNumbers (event)" required >'."</div>";
+			echo '<div class="mb" id ="add_books_2" >'."<strong>Name</strong>".'<input id ="ab_fields" type="text" name="name_author" placeholder="Author Name" required >'."</div>";
+			echo '<div class="mb" id ="add_books_2" >'."<strong>Nationality</strong>".'<input id ="ab_fields" type="text" name="nationality" placeholder="Nationality" required >'."</div>";
+			echo '<div>'.'<input id="menu_bts_1" type="submit" name="save_button" value="Add Author" >'."</div>";
+			echo '</form>';
+		?>
+	</body>
+</html>
